d77fba93d6c3922168bba1e2b39597eeaa03eff5
Salint <salintofficial@hotmail.com>
2020-07-15 02:50:46 +0200

    Added bacontext command

--- src/commands/bacontext.ts
+++ src/commands/bacontext.ts
@@ -3,8 +3,8 @@ import config from "../../config.json";
 import request from "request";
 
 const properties = {
-	name: "dadjoke",
-	description: "Dad joke during Quarantine",
+	name: "bacontext",
+	description: "Gibberish about bacon!",
 	args: []
 };
 
@@ -12,15 +12,16 @@ const run = (message: Message): number => {
 
 	const embed: MessageEmbed = new MessageEmbed();
 	embed.setColor(config.color as ColorResolvable);
-	embed.setTitle("Free dad joke.");
+	embed.setTitle("Bacon gibberish!");
 
 	request({
-		url: "https://icanhazdadjoke.com/",
+		url: "https://baconipsum.com/api/?type=all-meat&paras=1&format=text",
 		headers: {
 			"Accept": "text/plain",
 			"User-Agent": "request"
 		}
 	}, (error, response, body) => {
+		console.log(body);
 		embed.setDescription(body);
 		message.channel.send(embed);
 	});
