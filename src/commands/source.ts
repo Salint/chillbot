f1a2c7c9816553d7f80e79bfa46d75a41a8da101
Salint <salintofficial@hotmail.com>
2020-07-18 23:20:13 +0200

    Added source command

---
+++ src/commands/source.ts
@@ -0,0 +1,28 @@
+import { Message, MessageEmbed, ColorResolvable } from "discord.js";
+import config from "../../config.json";
+
+const properties = {
+	name: "source",
+	description: "Source and license of the bot.",
+	args: []
+};
+
+const run = (message: Message): number => {
+
+	const embed: MessageEmbed = new MessageEmbed();
+	embed.setColor(config.color as ColorResolvable);
+	embed.setTitle("Bot's source.");
+	embed.setDescription("ChillBot is an open-sourced bot, which means you're able to take a look \
+	at the source code, and even run your own version of the bot with your own commands. :)");
+	embed.addField("Source", "I've been created by Salint#5356.\nTherefore, my source code is available at GitHub: \
+	[Salint/chillbot](https://github.com/Salint/chillbot)");
+	embed.addField("License", "ChillBot is licensed under the [MIT license](https://github.com/Salint/chillbot/blob/master/LICENSE).");
+
+	message.channel.send(embed);
+	return 1;
+};
+
+/* eslint-disable */
+exports.properties = properties;
+exports.run = run;
+/* eslint-enable */
