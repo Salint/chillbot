10b30cadfcf3773f56937122d87c6e7e14ead1d6
Salint <salintofficial@hotmail.com>
2020-07-19 23:30:33 +0200

    Added invite command

---
+++ src/commands/invite.ts
@@ -0,0 +1,19 @@
+import { Message, MessageEmbed, ColorResolvable } from "discord.js";
+import config from "../../config.json";
+
+const properties = {
+	name: "invite",
+	description: "An invite link to the bot..",
+	args: []
+};
+
+const run = (message: Message): number => {
+
+	message.channel.send("https://discord.com/api/oauth2/authorize?client_id=681197286201491504&permissions=8&scope=bot");
+	return 1;
+};
+
+/* eslint-disable */
+exports.properties = properties;
+exports.run = run;
+/* eslint-enable */
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import events from "./events";
 
 const Bot: Client = new Client();
 
-
 Bot.on("ready", () => events.readyEvent(Bot));
 Bot.on("message", (message: Message) => events.messageEvent(Bot, message));
 
