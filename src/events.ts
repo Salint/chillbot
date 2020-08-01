b8f474e899d9add6ba431a831b823b4073645657
Salint <salintofficial@hotmail.com>
2020-08-02 00:54:45 +0200

    Refactor code style

--- src/events.ts
+++ src/events.ts
@@ -1,30 +1,29 @@
-import { Client, Message } from "discord.js";
+import { Message } from "discord.js";
 import config from "../config.json";
 import commands from "./loader";
+import Bot from "./main";
 
-const readyEvent = (Bot: Client): number => {
+const readyEvent = (): Promise<void> => {
 
 	Bot.user.setActivity(`in ${Bot.guilds.cache.array().length} guilds! ${config.prefix}help.`, { type: "PLAYING" });
 
 	console.log("Bot has started.");
 
-	return 1;
+	return;
 };
 
-const messageEvent = (message: Message): number => {
+const messageEvent = (message: Message): Promise<void> => {
 
 	const args = message.content.substr(config.prefix.length).split(" ");
 	const command = args[0].toLowerCase();
 
-	if(!message.content.startsWith(config.prefix)) return 1;
-	if(message.author.bot) return 1;
-	if(message.member == null) return 1;
-	if(!commands[command]) return 1;
+	if(!message.content.startsWith(config.prefix) || message.author.bot || message.member == null) return;
+	if(!commands[command]) return;
 
 	if(commands[command].properties.args.length == 0) commands[args[0].toLowerCase()].run(message);
 	else commands[command].run(message, args);
 
-	return 1;
+	return;
 };
 
 
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import events from "./events";
 
 const Bot: Client = new Client();
 
-Bot.on("ready", () => events.readyEvent(Bot));
+Bot.on("ready", () => events.readyEvent());
 Bot.on("message", (message: Message) => events.messageEvent(message));
 
 Bot.login(process.env.token);
+
+export default Bot;
