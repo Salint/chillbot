22e2d8c08ee2f24b478a6e0e5f89ff0ca89e70f6
Mohamed Abduljawad <salintofficial@hotmail.com>
2021-01-08 22:18:28 +0200

    Add support for .env files

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { Client, Message } from "discord.js";
 import events from "./events";
+import { config as env } from "dotenv";
 import update from "./update";
 
 const Bot: Client = new Client();
+
+if(!process.env.TOKEN)  {
+	env();
+}
+
 
 Bot.on("ready", () => events.readyEvent());
 Bot.on("message", (message: Message) => events.messageEvent(message));
 
 setInterval(() => update(Bot), 30000);
 
-Bot.login(process.env.token);
+Bot.login(process.env.TOKEN);
 
 export default Bot;
